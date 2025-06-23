fc705770d63078a3b52a1c68129b1d16043232f4
Alejandro <bo.aarana@gmail.com>
2025-06-23 15:45:53 -0400

    refactor: update conditional styling and add icon

--- src/components/form/SubmitButton.tsx
+++ src/components/form/SubmitButton.tsx
@@ -1,19 +1,45 @@
+"use client";
+
+import { cloneElement } from "react";
+
 import { Button } from "@/components/ui/button";
-import { LucideLoaderCircle } from "lucide-react";
+import { cn } from "@/lib/utils";
+import { LucideLoader2, type LucideProps } from "lucide-react";
 import { useFormStatus } from "react-dom";
 
 export default function SubmitButton({
   label,
+  icon,
   className,
+  ...props
 }: {
   label: string;
+  icon?: React.ReactElement;
   className?: string;
-}) {
+} & React.ComponentProps<typeof Button>) {
   const { pending: isPending } = useFormStatus();
   return (
-    <Button type="submit" disabled={isPending} className={className}>
-      {isPending && <LucideLoaderCircle className="mr-2 size-4 animate-spin" />}
+    <Button
+      type="submit"
+      disabled={isPending}
+      className={cn("gap-0", className)}
+      {...props}
+    >
+      {isPending && (
+        <LucideLoader2
+          className={cn("size-4 animate-spin", {
+            "mr-2": !!label,
+          })}
+        />
+      )}
+
       {label}
+
+      {isPending ? null : icon ? (
+        <span className={cn({ "pl-2": !!label })}>
+          {cloneElement(icon, { className: "size-4" } as LucideProps)}
+        </span>
+      ) : null}
     </Button>
   );
 }
