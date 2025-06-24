cba00265229c3f7214254a4548eb9f80dcf54dec
Alejandro <bo.aarana@gmail.com>
2025-06-24 16:04:14 -0400

    feat: add breadcrumb compnent

--- src/app/tickets/[ticketId]/edit/page.tsx
+++ src/app/tickets/[ticketId]/edit/page.tsx
@@ -1,6 +1,9 @@
+import { BreadCrumbs } from "@/components/Breadcrumbs";
 import CardCompact from "@/components/CardCompact";
+import { Separator } from "@/components/ui/separator";
 import TicketUpsertForm from "@/features/ticket/components/TicketUpsertForm";
 import { getTicket } from "@/features/ticket/queries/getTicket";
+import { ticketPath } from "@/routes";
 import { notFound } from "next/navigation";
 
 type Params = Promise<{ ticketId: string }>;
@@ -14,13 +17,23 @@ export default async function TicketEditPage(props: { params: Params }) {
   }
 
   return (
-    <div className="flex-1 flex flex-col justify-center items-center">
-      <CardCompact
-        className="w-full max-w-[420px] animate-fade-from-top"
-        title="Edit Ticket"
-        description="Edit your ticket"
-        content={<TicketUpsertForm ticket={ticket} />}
+    <div className="flex-1 flex flex-col gap-y-8">
+      <BreadCrumbs
+        breadcrumbs={[
+          { title: "Tickets", href: "/tickets" },
+          { title: ticket.title, href: ticketPath(ticket.id) },
+          { title: "Edit" },
+        ]}
       />
+      <Separator />
+      <div className="flex-1 flex flex-col justify-center items-center">
+        <CardCompact
+          className="w-full max-w-[420px] animate-fade-from-top"
+          title="Edit Ticket"
+          description="Edit your ticket"
+          content={<TicketUpsertForm ticket={ticket} />}
+        />
+      </div>
     </div>
   );
 }
--- src/app/tickets/[ticketId]/page.tsx
+++ src/app/tickets/[ticketId]/page.tsx
@@ -1,3 +1,5 @@
+import { BreadCrumbs } from "@/components/Breadcrumbs";
+import { Separator } from "@/components/ui/separator";
 import TicketItem from "@/features/ticket/components/TicketItem";
 import { getTicket } from "@/features/ticket/queries/getTicket";
 import { notFound } from "next/navigation";
@@ -13,8 +15,17 @@ export default async function TicketPage(props: { params: Params }) {
   }
 
   return (
-    <div className="flex justify-center animate-fade-from-top">
-      <TicketItem ticket={ticket} isDetail />
+    <div className="flex-1 flex flex-col gap-y-8">
+      <BreadCrumbs
+        breadcrumbs={[
+          { title: "Tickets", href: "/tickets" },
+          { title: ticket.title },
+        ]}
+      />
+      <Separator />
+      <div className="flex justify-center animate-fade-from-top">
+        <TicketItem ticket={ticket} isDetail />
+      </div>
     </div>
   );
 }
